7b25aeb36431b6cabf84358c1b38f89d74d55ff7
Mikhail <124215434+Mlosyakov@users.noreply.github.com>
2024-01-19 15:56:23 +1000

    Update inference.py

--- inference/inference.py
+++ inference/inference.py
@@ -19,12 +19,12 @@ from utils import get_project_dir, configure_logging
 
 DATA_DIR = os.path.abspath(os.path.join(ROOT_DIR, '../data'))
 if not os.path.exists(DATA_DIR):
-  os.makedirs(DATA_DIR)
+    os.makedirs(DATA_DIR)
 
 CONF_FILE = ".vscode/settings.json"
 logger.info("Getting few important dependencies...")
 with open(CONF_FILE, "r") as file:
-  configur = json.load(file)
+    configur = json.load(file)
 
 logger.info("Defining paths...")
 DATA_DIR = get_project_dir(configur["general"]["data_dir"])
@@ -34,45 +34,45 @@ TARGET_COL = configur["general"]["target_col"]
 DICT_NAME = configur["train"]["dict_name"]
 DICT_LOC = os.path.join(MODEL_PATH, DICT_NAME)
 def preprocess_inf(path):
-  df = pd.read_csv(url)
-  X = df.drop(columns = TARGET_COL).values
-  X = torch.tensor(X, dtype = torch.float32)
-  X = DataLoader(dataset = X, batch_size = len(X), num_workers=1)
-  return X
+    df = pd.read_csv(url)
+    X = df.drop(columns = TARGET_COL).values
+    X = torch.tensor(X, dtype = torch.float32)
+    X = DataLoader(dataset = X, batch_size = len(X), num_workers=1)
+    return X
 
 def load_model(folder_path):
-  model_files = list(Path(folder_path).rglob('*.pth'))
-  checkpoint_files = list(Path(folder_path).rglob('*ckpt'))
-
-  if not model_files or not checkpoint_files:
-    raise FileNotFoundError('No models or checkpoints found. Train model first')
-
-  latest_model_path = max(model_files, key = os.path.getctime)
-  latest_checkpoint_path = max(checkpoint_files, key = os.path.getctime)
-
-  if latest_model_path.parts[0].startswith('http'):
-    raise NotImplementedError('Please use only local models')
-  else:
-    model = torch.load(latest_model_path, map_location = torch.device('cpu'))
-
-  if latest_checkpoint_path.parts[0].startswith('http'):
-    raise NotImplementedError('Please use only local models')
-  else:
-    checkpoint = torch.load(latest_checkpoint_path, map_location = torch.device('cpu'))
-
-  model.load_state_dict(checkpoint['state_dict'])
-  return model
+    model_files = list(Path(folder_path).rglob('*.pth'))
+    checkpoint_files = list(Path(folder_path).rglob('*ckpt'))
+  
+    if not model_files or not checkpoint_files:
+      raise FileNotFoundError('No models or checkpoints found. Train model first')
+  
+    latest_model_path = max(model_files, key = os.path.getctime)
+    latest_checkpoint_path = max(checkpoint_files, key = os.path.getctime)
+  
+    if latest_model_path.parts[0].startswith('http'):
+      raise NotImplementedError('Please use only local models')
+    else:
+      model = torch.load(latest_model_path, map_location = torch.device('cpu'))
+  
+    if latest_checkpoint_path.parts[0].startswith('http'):
+      raise NotImplementedError('Please use only local models')
+    else:
+      checkpoint = torch.load(latest_checkpoint_path, map_location = torch.device('cpu'))
+  
+    model.load_state_dict(checkpoint['state_dict'])
+    return model
 
 def get_preds(model, dataloader):
-  model.eval()
-  prediction_list = []
-  for batch in dataloader:
-    inputs = batch
-    outputs = model(inputs)
-    _, predictions = torch.max(F.softmax(outputs, dim = 1), 1)
-    prediction_list.extend(predictions.detach().numpy())
-    label_decode = np.load(DICT_LOC,allow_pickle='TRUE').item()
-  return np.vectorize(label_decode.get)(prediction_list)
+    model.eval()
+    prediction_list = []
+    for batch in dataloader:
+      inputs = batch
+      outputs = model(inputs)
+      _, predictions = torch.max(F.softmax(outputs, dim = 1), 1)
+      prediction_list.extend(predictions.detach().numpy())
+      label_decode = np.load(DICT_LOC,allow_pickle='TRUE').item()
+    return np.vectorize(label_decode.get)(prediction_list)
 
 
 model = load_model(MODEL_PATH)
